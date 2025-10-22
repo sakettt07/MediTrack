897624fb0cb74705a9ec6813e9654d17f5ceeed3
saket <saket@saketgupta.com>
2025-10-23 00:58:15 +0530

    chore: update dependencies in package.json
    - upgraded expo from ~54.0.11 to 54.0.19
    - upgraded expo-image from ~3.0.8 to ~3.0.10
    - added expo-linear-gradient at ~15.0.7
    - upgraded expo-router from ~6.0.9 to ~6.0.13
    - upgraded expo-system-ui from ~6.0.7 to ~6.0.8
    - upgraded react-native from 0.81.4 to 0.81.5
    - added react-native-linear-gradient at ^2.8.3
    - downgraded react-native-svg from ^15.13.0 to 15.12.1

--- app/login/index.jsx
+++ app/login/index.jsx
@@ -1,30 +1,64 @@
-import { View, Text, Image,StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
 
 export default function Login() {
   return (
-    <View style={{
-      backgroundColor:"red",
-      height:"100%"
-    }}>
-      <View style={{
-        display:'flex',
-        alignItems:'center',
-        marginTop:80
-      }}>
-        <Image source={require("../../assets/loginImg/Intro.png")} style={styles?.image} />
+    <View
+      style={{
+        backgroundColor: "white",
+        height: "100%",
+      }}
+    >
+      <View
+        style={{
+          display: "flex",
+          alignItems: "center",
+          marginTop: 100,
+        }}
+      >
+        <Image
+          source={require("../../assets/loginImg/Intro.jpg")}
+          style={styles?.image}
+        />
       </View>
-      <Text style={{
-        color:"white",
-        fontSize:"30px"
-      }}>Stay on track , stay healthy</Text>
-    </View>
-  )
-};
+      <View style={{
+        padding:25,
+        backgroundColor:"#c6072add",
+        height:'100%'
+      }}>
+        <Text
+          style={{
+            color: "white",
+            marginTop: 23,
+            fontSize: 28,
+            textAlign: "center",
+            fontWeight:'bold'
+          }}
+        >
+          Stay on track, Stay healthy
+        </Text>
+        <Text
+          style={{
+            color: "white",
+            marginTop: 23,
+            fontSize: 14,
+            textAlign: "center",
+          }}
+        >
+          Track your meds, take control of your health.Stay consistent stay confident
+        </Text>
 
-const styles=StyleSheet.create({
-  image:{
-    width:370,
-    height:240
-  }
-})
+        <TouchableOpacity>
+          <Text>Continue</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 370,
+    height: 240,
+  },
+});
