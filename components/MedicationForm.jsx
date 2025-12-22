7d081f1e2acc3c64e87384fb2484ed969c72ecd5
saket <saket@saketgupta.com>
2025-12-23 00:55:33 +0530

    feat: enhance MedicationForm with input handling and add TypeList for medication types

--- components/MedicationForm.jsx
+++ components/MedicationForm.jsx
@@ -1,14 +1,95 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { TypeList } from "../constants/Options";
 
 const MedicationForm = () => {
+  const [formData, setFormData] = useState();
+
+  const onHandleInputChange = (field, value) => {
+    setFormData((prev) => ({
+      ...prev,
+      [field]: value,
+    }));
+    console.log(formData);
+  };
   return (
-    <View>
-      <View>
-        <Text>Medication</Text>
+    <View
+      style={{
+        padding: 20,
+      }}
+    >
+      <Text style={styles.header}>Add New Medication</Text>
+      <View style={styles.inputGroup}>
+        <Ionicons
+          style={styles.icon}
+          name="medkit-outline"
+          size={24}
+          color="black"
+        />
+        <TextInput
+          onChangeText={(value) => onHandleInputChange("name", value)}
+          style={styles.textInput}
+          placeholder="Medicine Name"
+        />
       </View>
+      {/* Type List */}
+      <FlatList
+        data={TypeList}
+        horizontal
+        style={{
+          marginTop: 20,
+        }}
+        renderItem={({ item, index }) => (
+          <TouchableOpacity
+            onPress={() => onHandleInputChange("type", item)}
+            style={[styles.inputGroup, { marginRight: 3 }]}
+          >
+            <Text style={styles.typeText}>{item?.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
+      <Text>Okkkjk</Text>
     </View>
   );
 };
 
 export default MedicationForm;
+
+const styles = StyleSheet.create({
+  header: {
+    fontSize: 20,
+    fontWeight: 700,
+  },
+  inputGroup: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 12,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "#d6d4d4ff",
+    marginTop: 7,
+  },
+  textInput: {
+    flex: 1,
+    marginLeft: 10,
+    fontSize: 16,
+  },
+  typeText: {
+    fontSize: 13,
+  },
+  icon: {
+    color: "#7fa8eaff",
+    borderRightWidth: 1,
+    paddingRight: 12,
+    borderColor: "rgba(179, 179, 176, 0.93)",
+  },
+});
---
+++ constants/Options.jsx
@@ -0,0 +1,26 @@
+export const TypeList = [
+  {
+    name: "Tablet",
+    icon: "something",
+  },
+  {
+    name: "Capsules",
+    icon: "something",
+  },
+  {
+    name: "Drops",
+    icon: "something",
+  },
+  {
+    name: "Syrup",
+    icon: "something",
+  },
+  {
+    name: "Injection",
+    icon: "something",
+  },
+  {
+    name: "Other",
+    icon: "something",
+  },
+];
